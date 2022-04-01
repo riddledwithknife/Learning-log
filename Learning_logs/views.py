0f89e424a8a8b956149d22ff13845b891dafdf43
Daniil Zharuk <riddledwithknife@icloud.com>
2022-04-01 21:28:26 +0300

    Second stage 2

--- Learning_logs/views.py
+++ Learning_logs/views.py
@@ -60,7 +60,7 @@ def edit_entry(request, entry_id):
         form = EntryForm(instance=entry, data=request.POST)
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+        return HttpResponseRedirect(reverse('Learning_logs:topic', args=[topic.id]))
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'Learning_logs/edit_entry.html', context)
